527703f8ae9ddf0067e78a8ef843ea388ac0b37d
Jasmine Vary <vary0005@algonquinlive.com>
2019-06-21 12:19:55 -0400

    initial commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="index" @click="todoDone(index)">
+      {{ todo }}
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    todoDone(index) {
+      console.log(index);
+      // $emit emits an event from the child component to the parent compoenent
+      this.$emit("removeTodo", index);
+    }
+  }
+};
+</script>
---
+++ src/views/TodoList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <TodoForm @submit.prevent="addTodo">
+      <h2 slot="title">Add a Todo</h2>
+      <p slot="desc">Your Todo will be saved</p>
+    </TodoForm>
+    <Todo :todos="todoList" @removeTodo="appDeleteTodo" />
+  </div>
+</template>
+<script>
+import Todo from "@/components/Todo.vue";
+import TodoForm from "@/components/TodoForm.vue";
+
+export default {
+  data() {
+    return {
+      todoList: ["Walk the dogs", "Go for a ride"]
+    };
+  },
+  components: {
+    Todo,
+    TodoForm
+  },
+  methods: {
+    appDeleteTodo(index) {
+      this.todoList.splice(index, 1);
+    },
+    addTodo(todo) {
+      this.todoList.push(todo);
+    }
+  }
+};
+</script>
+
+<style>
+ul {
+  list-style: none;
+  width: 50%;
+  margin: 0 auto;
+}
+ul li {
+  border-bottom: 1px solid #acacac;
+  padding: 10px 0;
+  margin-bottom: 10px;
+}
+</style>
