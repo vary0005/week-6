3af9148066a3cf9bb89dbe4cdc81d270e8d1b1a5
Jasmine Vary <vary0005@algonquinlive.com>
2019-06-28 12:19:09 -0400

    preparing for deployment

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Todo List</h1>
-    <TodoForm @submit.prevent="addTodo">
+    <TodoForm @newTodo="addTodo">
       <h2 slot="title">Add a Todo</h2>
       <p slot="desc">Your Todo will be saved</p>
     </TodoForm>
@@ -11,11 +11,12 @@
 <script>
 import Todo from "@/components/Todo.vue";
 import TodoForm from "@/components/TodoForm.vue";
+import axios from "axios";
 
 export default {
   data() {
     return {
-      todoList: ["Walk the dogs", "Go for a ride"]
+      todoList: []
     };
   },
   components: {
@@ -25,10 +26,39 @@ export default {
   methods: {
     appDeleteTodo(index) {
       this.todoList.splice(index, 1);
+      axios.put(
+        "https://vary0005-vue-and-axios.firebaseio.com/data.json",
+        this.todoList
+      );
     },
     addTodo(todo) {
       this.todoList.push(todo);
+      axios
+        .put(
+          "https://vary0005-vue-and-axios.firebaseio.com/data.json",
+          this.todoList
+        )
+        .then(response => {
+          console.log(response);
+          console.log("Your data was saved status: " + response.status);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
+  },
+  created() {
+    axios
+      .get("https://vary0005-vue-and-axios.firebaseio.com/data")
+      .then(response => {
+        console.log(response.data);
+        {
+          if (response.data) this.todoList = response.data;
+        }
+      })
+      .catch(error => {
+        console.log("There was an error in getting data: " + error.response);
+      });
   }
 };
 </script>
